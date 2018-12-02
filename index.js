2e06d4b382d2cb14b072e6bc325cf9ecb56e2bcd
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 03:39:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+var returnFirstTwoDrivers = function()
+{
+  return returnFirstTwoDrivers = drivers.slice(0,2)
+}
+
+var returnLastTwoDrivers = function()
+{
+  return drivers.slice(-2)
+}
