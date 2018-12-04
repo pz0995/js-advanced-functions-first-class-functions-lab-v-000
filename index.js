fb550c0d5ac3dfe994abb10346eebe039bae872f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-04 11:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+var selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+var returnFirstTwoDrivers = function(drivers)
+{
+  return returnFirstTwoDrivers = drivers.slice(0,2)
+}
+
+var returnLastTwoDrivers = function()
+{
+  return drivers.slice(-2)
+}
+
+function createFareMultiplier() {
+
+
+}
+function fareDoubler(fare) {
+  
+  
+}
+
+
+function fareTripler(fare) {
+  
+}
+
+function fetchSpecifiedDrivers(drivers, returnLastTwoDrivers) {
+  
+  
+}
